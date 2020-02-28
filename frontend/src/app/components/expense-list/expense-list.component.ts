945bb372aa527acc52a84e65f09eb8187c7a1b5d
Beiser Johannes <jb191@hdm-stuttgart.de>
2020-02-28 04:11:10 +0100

    Expense-list now animated

--- frontend/src/app/components/expense-list/expense-list.component.ts
+++ frontend/src/app/components/expense-list/expense-list.component.ts
@@ -4,9 +4,26 @@ import { CategoryService } from 'src/app/services/category/category.service';
 import { SliderService } from 'src/app/services/slider/slider.service';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ExpenseBottomSheetComponent } from './expense-bottom-sheet/expense-bottom-sheet.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-expense-list',
+  animations: [
+    trigger('slideInOut', [
+      state('out', style({
+        opacity: '0',
+        overflow: 'hidden',
+        height: '0px',
+      })),
+      state('in', style({
+        opacity: '1',
+        overflow: 'hidden',
+        height: '*',
+      })),
+      transition('out => in', animate('150ms ease-in-out')),
+      transition('in => out', animate('150ms ease-in-out'))
+    ])
+  ],
   templateUrl: './expense-list.component.html',
   styleUrls: ['./expense-list.component.less']
 })
@@ -23,15 +40,31 @@ export class ExpenseListComponent implements OnInit {
   public detailViewShownForIndex: number;
 
   ngOnInit(): void {
+
   }
 
-  public toggleDetailView(index: number) {
-    if (this.detailViewShownForIndex != null && this.detailViewShownForIndex == index) {
-      this.detailViewShownForIndex = null;
-    } else {
-      this.detailViewShownForIndex = index;
+  helper = {}
+  helpMenuOpenForIndex(index: number):string{
+    return this.helper[index] || 'out';
+  }
+
+  toggleHelpMenu(index: number): void {
+    if(this.helper[index]){
+      //already exists --> is open
+      this.helper = {};
+    }else{
+      this.helper = {};
+      this.helper[index] =  'in';
     }
   }
+
+  // public toggleDetailView(index: number) {
+  //   if (this.detailViewShownForIndex != null && this.detailViewShownForIndex == index) {
+  //     this.detailViewShownForIndex = null;
+  //   } else {
+  //     this.detailViewShownForIndex = index;
+  //   }
+  // }
 
   openBottomSheet(e:MouseEvent, expense: any): void {
     e.stopPropagation();
