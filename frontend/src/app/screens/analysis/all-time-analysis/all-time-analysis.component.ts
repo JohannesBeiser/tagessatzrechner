0dff109925ca53a7f4a2a401b339c4553d79e4d6
Beiser Johannes <jb191@hdm-stuttgart.de>
2021-11-25 03:38:44 +0100

    Column chart added to All-time-analysis

--- frontend/src/app/screens/analysis/all-time-analysis/all-time-analysis.component.ts
+++ frontend/src/app/screens/analysis/all-time-analysis/all-time-analysis.component.ts
@@ -1,10 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { differenceInDays } from 'date-fns';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { Expense, ExpenseService } from 'src/app/services/expenses/expense.service';
 import { GroupsService } from 'src/app/services/groups/groups.service';
+import * as Highcharts from 'highcharts';
+
+
+type Stats =  {
+  amountOfDays: number,
+  averagePerMonth: number,
+  averagePerYear: number,
+  averageperDay: number,
+  total: number, // inirial calculation
+  totalRecurring: number, // initial calc
+  yearsData: {
+    year: number, // initial calc
+    firstDate: Date,// initial calc
+    lastDate: Date,// initial calc
+    amountInDays: number,
+    total: number,// initial calc
+    totalRecurring: number,// initial calc
+    averagePerMonth: number,
+  }[],
+  categoryYearsData: {
+    category: number,
+    data: {
+      year: number,
+      total: number,// initial calc
+    }[]
+  }[]
+}
 
 @Component({
   selector: 'app-all-time-analysis',
@@ -19,37 +46,18 @@ export class AllTimeAnalysisComponent implements OnInit {
     private categoryService: CategoryService,
   ) { }
 
-  public expenses$ : Observable<Expense[]>;
+  public expenses$: Observable<Expense[]>;
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {};
+  chartReady: boolean = false;
+
+  
 
   // this Construct is the exact result of all of the expenses and gets build so its easier for the template and charts to display.
   //Everything thats needed is already pre-calculated in here
-  public stats: {
-    amountOfDays: number,
-    averagePerMonth: number,
-    averagePerYear: number,
-    averageperDay: number,
-    total: number, // inirial calculation
-    totalRecurring: number, // initial calc
-    yearsData: {
-      year: number, // initial calc
-      firstDate: Date,// initial calc
-      lastDate: Date,// initial calc
-      amountInDays: number,
-      total: number,// initial calc
-      totalRecurring: number,// initial calc
-      averagePerMonth: number,
-    }[],
-    categoryYearsData: {
-      category: number,
-      data: {
-        year: number,
-        total: number,// initial calc
-      }[]
-    }[]
-  }
+  public stats: Stats;
 
   ngOnInit(): void {
-
     this.stats = {
       amountOfDays: 0,
       averagePerMonth: 0,
@@ -61,78 +69,76 @@ export class AllTimeAnalysisComponent implements OnInit {
       yearsData: [],
     }
 
-    this.expenses$ = this.expenseService.getExpenses("expenses").pipe(filter(expenses=> expenses.length>0))
+    this.expenses$ = this.expenseService.getExpenses("expenses").pipe(filter(expenses => expenses.length > 0))
 
-    this.expenses$.subscribe(expenses=>{
-
+    this.expenses$.subscribe(expenses => {
       let firstDate: Date;
       let lastDate: Date;
 
-      expenses.forEach(expense=>{
-      
-        //TODO: Ignore dates and expenses which date > current date
-        if(new Date(expense.date) < new Date()){
-          if(!firstDate){
-            firstDate=  new Date(expense.date);
-          }
-          if(!lastDate){
-            lastDate=  new Date(expense.date);
-          }
-  
-          if(new Date(expense.date)<firstDate){
+      expenses.forEach(expense => {
+        if (new Date(expense.date) < new Date()) {
+          if (!firstDate) {
             firstDate = new Date(expense.date);
           }
-  
-          if(new Date(expense.date)>lastDate){
+
+          if (!lastDate) {
             lastDate = new Date(expense.date);
           }
-  
+
+          if (new Date(expense.date) < firstDate) {
+            firstDate = new Date(expense.date);
+          }
+
+          if (new Date(expense.date) > lastDate) {
+            lastDate = new Date(expense.date);
+          }
+
           // all totals
-          this.stats.total+= expense.amount;
-          if(expense.recurring){
+          this.stats.total += expense.amount;
+          if (expense.recurring) {
             this.stats.totalRecurring += expense.amount;
           }
-  
+
           // years total
-          let expenseYear: number =  new Date(expense.date).getFullYear();
-          let yearsDataMatch = this.stats.yearsData.find(el=>el.year === expenseYear);
-          if(!yearsDataMatch){
-            this.stats.yearsData.push({year: expenseYear, total: 0, totalRecurring: 0, averagePerMonth: 0, amountInDays: null, firstDate: null, lastDate: null});
+          let expenseYear: number = new Date(expense.date).getFullYear();
+          let yearsDataMatch = this.stats.yearsData.find(el => el.year === expenseYear);
+          if (!yearsDataMatch) {
+            this.stats.yearsData.push({ year: expenseYear, total: 0, totalRecurring: 0, averagePerMonth: 0, amountInDays: null, firstDate: null, lastDate: null });
           }
-          yearsDataMatch = this.stats.yearsData.find(el=>el.year === expenseYear);
+          yearsDataMatch = this.stats.yearsData.find(el => el.year === expenseYear);
           yearsDataMatch.total += expense.amount;
-          if(!yearsDataMatch.firstDate){
-            yearsDataMatch.firstDate=  new Date(expense.date);
-          }
-          if(!yearsDataMatch.lastDate){
-            yearsDataMatch.lastDate=  new Date(expense.date);
-          }
-  
-          if(new Date(expense.date)<yearsDataMatch.firstDate){
+          if (!yearsDataMatch.firstDate) {
             yearsDataMatch.firstDate = new Date(expense.date);
           }
-  
-          if(new Date(expense.date)>yearsDataMatch.lastDate){
+          if (!yearsDataMatch.lastDate) {
             yearsDataMatch.lastDate = new Date(expense.date);
           }
-  
-          if(expense.recurring){
+
+          if (new Date(expense.date) < yearsDataMatch.firstDate) {
+            yearsDataMatch.firstDate = new Date(expense.date);
+          }
+
+          if (new Date(expense.date) > yearsDataMatch.lastDate) {
+            yearsDataMatch.lastDate = new Date(expense.date);
+          }
+
+          if (expense.recurring) {
             yearsDataMatch.totalRecurring += expense.amount;
           }
-  
+
           // years categroy totals
           let expenseCategory: number = expense.category;
           // find the category stats object whch matches the expenses category
-          let yearsCategoryMatch = this.stats.categoryYearsData.find(el=>el.category === expenseCategory);
-          if(!yearsCategoryMatch){
-            this.stats.categoryYearsData.push({category: expenseCategory, data: []});
+          let yearsCategoryMatch = this.stats.categoryYearsData.find(el => el.category === expenseCategory);
+          if (!yearsCategoryMatch) {
+            this.stats.categoryYearsData.push({ category: expenseCategory, data: [] });
           }
-          yearsCategoryMatch = this.stats.categoryYearsData.find(el=>el.category === expenseCategory);
-          let yearsCategoryDataMatch = yearsCategoryMatch.data.find(el=>el.year == expenseYear);
-          if(!yearsCategoryDataMatch){
-            yearsCategoryMatch.data.push({total: 0, year: expenseYear});
+          yearsCategoryMatch = this.stats.categoryYearsData.find(el => el.category === expenseCategory);
+          let yearsCategoryDataMatch = yearsCategoryMatch.data.find(el => el.year == expenseYear);
+          if (!yearsCategoryDataMatch) {
+            yearsCategoryMatch.data.push({ total: 0, year: expenseYear });
           }
-          yearsCategoryDataMatch = yearsCategoryMatch.data.find(el=>el.year == expenseYear);
+          yearsCategoryDataMatch = yearsCategoryMatch.data.find(el => el.year == expenseYear);
           yearsCategoryDataMatch.total += expense.amount;
         }
       });
@@ -142,14 +148,81 @@ export class AllTimeAnalysisComponent implements OnInit {
       this.stats.averageperDay = this.stats.total / this.stats.amountOfDays;
       this.stats.averagePerYear = this.stats.averageperDay * 365;
       this.stats.averagePerMonth = this.stats.averageperDay * 30.437; // average month = 30.437
-      this.stats.yearsData.forEach(yearData=>{
+      this.stats.yearsData.forEach(yearData => {
         yearData.amountInDays = differenceInDays(yearData.lastDate, yearData.firstDate);
         yearData.averagePerMonth = (yearData.total / yearData.amountInDays) * 30.437;
-      })
+      });
 
-      debugger;
-
+      this.initializeChart();
     });
+  }
+
+
+  private initializeChart() {
+
+    let years: string[] = this.stats.yearsData.sort((a,b)=>{return a.year - b.year}).map(el=>el.year.toString())
+
+    let values: number[] = this.stats.yearsData.sort((a,b)=>{return a.year - b.year}).map(el=>Math.round(el.total));
+
+    this.chartOptions = {
+      chart: {
+        type: 'column',
+        backgroundColor: 'transparent',
+      },
+      credits: {
+        enabled: false
+      },
+      title: {
+        text: ''
+      },
+      legend: {
+        enabled: false
+      },
+      xAxis: {
+        categories: years,
+        crosshair: true
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: ''
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' €';
+          }
+        }
+      },
+      tooltip: { enabled: false },
+      plotOptions: {
+        column: {
+          pointPadding: 0,
+          borderWidth: 0,
+          dataLabels: {
+            enabled: true,
+            align: 'center',
+            formatter: function () {
+              return this.y + ' €';
+            }
+          },
+        },
+        series: {
+          states: {
+              hover: {
+                  enabled: false,
+              },
+          }
+      }
+      },
+      series: [
+      {
+        color: 'tomato',
+        name: '',
+        type: 'column',
+        data: values
+      }]
+    };
+    this.chartReady = true; // needed so template doesnt try to initialize before its ready because it gets initialized asynchronously in subscribtion of expenses$
   }
 
 }
